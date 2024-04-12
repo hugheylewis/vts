ef9469d06a7734a80d5aaf8fb6cc44223c6fc2a3
hugheylewis <103709866+hugheylewis@users.noreply.github.com>
2024-04-12 16:34:03 -0400

    Create main.py

---
+++ main.py
@@ -0,0 +1,24 @@
+import csv
+import requests
+import re
+
+with open('query_data.csv') as qdata:
+    data = csv.reader(qdata, delimiter=',')
+    next(data, None)
+    for row in data:
+        url = f"https://www.virustotal.com/api/v3/domains/{row[0]}"
+
+        headers = {
+            "accept": "application/json",
+            "x-apikey": "API_KEY_HERE"
+        }
+
+        response = requests.get(url, headers=headers)
+        pattern = r'"malicious"\s*:\s*(\d+)'
+        response_data = response.text
+        match = re.search(pattern, response_data)
+
+        if match:
+            print(f"VirusTotal score for {row[0]}: {match.group(1)}")
+
+
